9325a959fad107a13cf8f4ef67da5d254c3fb217
Prathyush Potluri <prathyush.p16@iiits.in>
2019-08-25 10:08:53 +0530

    Update UCF101_loader.py

--- keras_video_classifier/library/utility/ucf/UCF101_loader.py
+++ keras_video_classifier/library/utility/ucf/UCF101_loader.py
@@ -35,7 +35,7 @@ def download_ucf(data_dir_path):
 
 
 def scan_ucf(data_dir_path, limit):
-    input_data_dir_path = data_dir_path + '/UCF-101'
+    input_data_dir_path = data_dir_path + '/UCF-Anomaly-Detection-Dataset'
 
     result = dict()
 
@@ -53,7 +53,7 @@ def scan_ucf(data_dir_path, limit):
 
 
 def scan_ucf_with_labels(data_dir_path, labels, hard_split = False):
-    input_data_dir_path = data_dir_path + '/UCF-101'
+    input_data_dir_path = data_dir_path + '/UCF-Anomaly-Detection-Dataset'
     if (hard_split == True):
         input_data_dir_path = data_dir_path + '/UCF-101_Train'
 
@@ -72,13 +72,14 @@ def scan_ucf_with_labels(data_dir_path, labels, hard_split = False):
 
 
 def load_ucf(data_dir_path):
-    UFC101_data_dir_path = data_dir_path + "/UCF-101"
+    UFC101_data_dir_path = data_dir_path + "/UCF-Anomaly-Detection-Dataset"
     if not os.path.exists(UFC101_data_dir_path):
-        download_ucf(data_dir_path)
+        #download_ucf(data_dir_path)
+        print("data dir not set properly")
 
 
 def main():
-    data_dir_path = '../very_large_data'
+    data_dir_path = '/content/drive/My Drive'
     load_ucf(data_dir_path)
 
 
